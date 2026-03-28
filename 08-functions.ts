0d309c6e5f32e42b09ec28f0a11343d89054a9d0
LoweDavid02 <louiecamarines21@gmail.com>
2026-03-28 12:59:09 +0800

    Done playing and updating the 08-functions.ts file

--- 08-functions.ts
+++ 08-functions.ts
@@ -1,62 +1,10 @@
-(() => {
+function introduceLowe(name: string, role: string): string {
+    return `Hello! I am ${name}, an aspiring ${role}.`;
+}
 
-    // Function Return Value Types
-    function add(a: number, b: number): number { // you can omit the function type since typescript can infers it
-        return a + b
-    }
+const intro = introduceLowe("Lowe David Tubat", "Mobile Application Developer");
+console.log(intro);
 
-    add(5, 10)
-    // add("5", 10)
-
-    // Special Type `void` - return nothing
-    // Function doesn't return a meaningful value, but it does finish
-    function log(message: string): void {
-        console.log(message)
-    }
-
-    log("Hello, World!")
-
-    // Another Special Type `never`
-    // Function never finishes normally (throws error, infinite loop)
-    function logAndThrow(errorMessage: string) {
-        console.log(errorMessage)
-        throw new Error(errorMessage)
-    }
-
-    // Function as Types
-    // Useful for callback functions
-    // () => {} is not same as () => void
-    // () => {} - arrow function
-    // () => void - function type
-    function performJob(cb: (m: string) => void) {  // you can also use cb: Function but () => void is the best practice
-        // ...
-        cb('Job Done!')
-    }
-
-    let logMsg = (msg: string): void => {
-        console.log(msg)
-    }
-
-    performJob(logMsg)
-
-    // Useful for objects and methods
-    type User = {
-        name: string;
-        age: number;
-        greet: () => string;
-    }
-
-    let user: User = {
-        name: "David",
-        age: 25,
-        // greet: function() {
-        //     return this.name
-        // }
-        greet() {
-            console.log(`Welcome, ${this.name}!`)
-            return this.name
-        }
-    }
-
-    user.greet()
-})
+const addSkill = (skills: string[], newSkill: string): number => {
+    return skills.push(newSkill);
+};
